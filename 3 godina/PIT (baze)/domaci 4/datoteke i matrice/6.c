e50c604fe538adb0e2bfc6f0094617d0593c51f3
Strahinja Djordjevic <strahinjadj08@gmail.com>
2025-11-18 10:53:39 +0100

    -test .gitignore

--- 3 godina/PIT (baze)/domaci 4/datoteke i matrice/6.c
+++ 3 godina/PIT (baze)/domaci 4/datoteke i matrice/6.c
@@ -1,2 +1,12 @@
 //U prvom redu datoteke se nalazi broj kosarkasa jednog kluba i broj odigranih utakmica u sezoni.
 //Napisati program kojim se izracunava prosecan broj postignutih koseva svakog igraca
+#include <stdio.h>
+
+int main(){
+    FILE *f=fopen("C:\\Users\\strahinja\\Desktop\\ETS-Rade-Koncar-III9\3 godina\\PIT (baze)\\domaci 4\\datoteke i matrice\\6-8.txt","r");
+    
+
+
+    fclose(f);
+    return 0;
+}
